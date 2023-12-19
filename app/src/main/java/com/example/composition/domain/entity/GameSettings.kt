aa6803d9bfd1c26716e130448ab5b650c0b85229
Evgeniy Gorovoy <kaknazvattebya@gmail.com>
2023-12-19 21:25:12 +0700

    add data and presentation layer

---
+++ app/src/main/java/com/example/composition/domain/entity/GameSettings.kt
@@ -0,0 +1,9 @@
+package com.example.composition.domain.entity
+
+data class GameSettings(
+    val maxSunValue: Int,
+    val minCountOfRightAnswers: Int,
+    val minPercentOfRightAnswers: Int,
+    val gameTimeInSeconds: Int
+)
+
